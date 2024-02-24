dba5717f7aff9b897b8ede34ef0cb4cd918c275f
Piyush Aaryan <piyushaaryan011@gmail.com>
2024-02-25 03:22:12 +0530

    Update fooocus.py

--- Code/fooocus.py
+++ Code/fooocus.py
@@ -3,10 +3,9 @@ import requests
 class RunpodAPI:
     def __init__(self, api_key):
         # self.base_url = "https://api.runpod.ai/v2/j6ctvdmtlle2lt/"
-        self.base_url = "https://api.runpod.ai/v2/fk64wqmhvr4f2w/"
+        self.base_url = "https://3y7wkrmlouh1fu-8888.proxy.runpod.net/"
         self.headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
         }
 
     def run_job(self, input_data):
